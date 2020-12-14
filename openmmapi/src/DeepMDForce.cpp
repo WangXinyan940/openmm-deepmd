0076d4a8b92de0ca168839556a09fc508d341bde
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-14 12:34:13 +0800

    Update DeepMDForce.cpp

--- openmmapi/src/DeepMDForce.cpp
+++ openmmapi/src/DeepMDForce.cpp
@@ -8,7 +8,7 @@ using namespace DeepMDPlugin;
 using namespace OpenMM;
 using namespace std;
 
-DeepMDForce::DeepMDForce(const std::string& filename, bool ifDouble) : dpfile(filename), usePeriodic(false), useDouble(ifDouble) {
+DeepMDForce::DeepMDForce(std::string& filename, bool ifDouble) : dpfile(filename), usePeriodic(false), useDouble(ifDouble) {
 }
 
 void setFile(std::string& filename) {
