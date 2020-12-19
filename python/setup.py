c8b4a49ca09ca848f0dedfd5051106cf8e06f731
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-19 23:08:13 +0800

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -7,6 +7,8 @@ import platform
 openmm_dir = '@OPENMM_DIR@'
 deepmd_plugin_header_dir = '@DEEPMD_PLUGIN_HEADER_DIR@'
 deepmd_plugin_library_dir = '@DEEPMD_PLUGIN_LIBRARY_DIR@'
+deepmd_lib_header_dir = '@DEEPMD_DIR@/include'
+deepmd_lib_library_dir = '@DEEPMD_DIR@/lib'
 
 # setup extra compile and link arguments on Mac
 extra_compile_args = ['-std=c++11']
@@ -19,8 +21,8 @@ if platform.system() == 'Darwin':
 extension = Extension(name='_openmmdeepmd',
                       sources=['DeepMDPluginWrapper.cpp'],
                       libraries=['OpenMM', 'OpenMMDEEPMD'],
-                      include_dirs=[os.path.join(openmm_dir, 'include'), deepmd_plugin_header_dir],
-                      library_dirs=[os.path.join(openmm_dir, 'lib'), deepmd_plugin_library_dir],
+                      include_dirs=[os.path.join(openmm_dir, 'include'), deepmd_plugin_header_dir, deepmd_lib_header_dir],
+                      library_dirs=[os.path.join(openmm_dir, 'lib'), deepmd_plugin_library_dir, deepmd_lib_library_dir],
                       runtime_library_dirs=[os.path.join(openmm_dir, 'lib')],
                       extra_compile_args=extra_compile_args,
                       extra_link_args=extra_link_args
