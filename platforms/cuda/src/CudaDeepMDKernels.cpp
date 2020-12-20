a4135ac79074ba6f28e9ee0a266eaf5a0aa02664
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-20 15:34:28 +0800

    Update CudaDeepMDKernels.cpp

--- platforms/cuda/src/CudaDeepMDKernels.cpp
+++ platforms/cuda/src/CudaDeepMDKernels.cpp
@@ -44,28 +44,10 @@ void CudaCalcDeepMDForceKernel::initialize(const System& system, const DeepMDFor
     cout << CudaDeepMDKernelSources::deepMDForce << endl;
     addForcesKernel = cu.getKernel(module, "addForces");
     testKernel = cu.getKernel(module, "testForces");
-
-    vector<float> testi;
-    vector<float> testj;
-    vector<float> testk(50,0);
-    for(int i=0;i<50;i++){
-        testi.push_back(i+1);
-        testj.push_back(3*i-1);
-    }
-    CudaArray inpi, inpj, inpk;
-    inpi.initialize(cu, 50, sizeof(float), "inpi");
-    inpj.initialize(cu, 50, sizeof(float), "inpj");
-    inpk.initialize(cu, 50, sizeof(float), "inpk");
-    inpi.upload(testi);
-    inpj.upload(testj);
-    inpk.upload(testk);
-    void* argtest[] = {&inpi.getDevicePointer(), &inpj.getDevicePointer(), &inpk.getDevicePointer()};
-    cout << "before send" << endl;
-    cu.executeKernel(testKernel, argtest, 10);
-
 }
 
 double CudaCalcDeepMDForceKernel::execute(ContextImpl& context, bool includeForces, bool includeEnergy) {
+    cu.setAsCurrent();
     vector<Vec3> pos;
     context.getPositions(pos);
     int numParticles = cu.getNumAtoms();
