a3b35671a118c6a68e0919c9daeb298d8d1d4fb8
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-20 12:03:34 +0800

    Update CudaDeepMDKernels.cpp

--- platforms/cuda/src/CudaDeepMDKernels.cpp
+++ platforms/cuda/src/CudaDeepMDKernels.cpp
@@ -40,6 +40,7 @@ void CudaCalcDeepMDForceKernel::initialize(const System& system, const DeepMDFor
         networkForces.initialize(cu, 3*numParticles, sizeof(float), "networkForces");
     #endif
     CUmodule module = cu.createModule(CudaDeepMDKernelSources::deepMDForce, defines);
+    cout << CudaDeepMDKernelSources::deepMDForce << endl;
     addForcesKernel = cu.getKernel(module, "addForces");
 }
 
