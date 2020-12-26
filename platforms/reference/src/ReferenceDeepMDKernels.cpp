d973d23afc3d5b6fcbe39d9d2cab64da30d48c66
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-12-26 22:06:34 +0800

    Update ReferenceDeepMDKernels.cpp

--- platforms/reference/src/ReferenceDeepMDKernels.cpp
+++ platforms/reference/src/ReferenceDeepMDKernels.cpp
@@ -47,7 +47,7 @@ void ReferenceCalcDeepMDForceKernel::initialize(const System& system, const Deep
     types = force.getType();
     usePeriodic = force.usesPeriodicBoundaryConditions();
     // save cutoff of graph
-    rcut = deepmodel.cutoff()*0.1;
+    rcut = deepmodel.cutoff();
     neighborList = NeighborList();
     ex.resize(numParticles);
 }
