b20e76ed0efa3fb9ed072636ba3478ceb14a7560
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2021-05-07 12:02:37 +0800

    Update DeepMDKernels.h

--- openmmapi/include/DeepMDKernels.h
+++ openmmapi/include/DeepMDKernels.h
@@ -5,7 +5,7 @@
 #include "openmm/KernelImpl.h"
 #include "openmm/Platform.h"
 #include "openmm/System.h"
-#include "deepmd/DeepPot.h"
+#include "deepmd/NNPInter.h"
 #include <string>
 
 namespace DeepMDPlugin {
